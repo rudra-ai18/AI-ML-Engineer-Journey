65f0aaf7f60e4f84fbb682b716d046dab3afb806
Rudra Pratap Singh Chauhan <rudrap404@gmail.com>
2026-08-01 11:56:29 +0530

    Add Week 2 conditionals practice

---
+++ 01-Python-Fundamentals/Learnings/Week-02/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+# 6. Take your age and check if it is greater than 18.v
+
+# age_ = int(input("Enter Age:"))
+# criteria_ = age_ > 18
+# print("I'm adult:", criteria_)
---
+++ 01-Python-Fundamentals/Learnings/Week-02/week-02.py
@@ -0,0 +1,98 @@
+# # Boolean & Comparison Operators
+
+# # 1. Take two numbers and check whether they are equal.
+
+# num1 = int(input("First Number:"))
+# num2 = int(input("Second Number:"))
+# num3 = num1 == num2 
+# print(num3)
+
+# # 2. Check whether two numbers are different.
+
+# num_1 = int(input("First Number"))
+# num_2 = int(input("Second Number"))
+# num3 = num_1 != num_2
+# print(num3)
+
+# # 3.  Check whether the first number is greater than the second.
+
+# nam_ = int(input("First Number:"))
+# nam_1 = int(input("Second Number:"))
+# print(nam_ > nam_1)
+
+# # 4. Check whether the first number is greater than or equal to the second
+
+# num0 = int(input("First Number:"))
+# num1 = int(input("Second Number:"))
+# print(num0 >= num1)
+
+# # 5. Print the result of 15 < 8
+
+# num1  = 15
+# num2 = 8
+# print(15 < 8)
+
+# # 6. Take your age and check if it is greater than 18.v
+
+# age_ = int(input("Enter Age:"))
+# criteria_ = age_ > 18
+# print("I'm adult:", criteria_)
+
+# # 7. Take your marks and check if they are greater than 40.
+
+# marks_ = int(input("Enter Marks"))
+# criteria_ = marks_ > 40
+# print(criteria_)
+
+# # 8. Take your height and check if it is greater than 6 feet.
+
+# height_ = int(input("Enter Height:"))
+# criteria_ = height_ > 6
+# print(criteria_)
+
+# # 9. Take two floating-point numbers and compare them.
+
+# num1 = float(input("Enter First Float number:"))
+# num2 = float(input("Enter Second Float Number:"))
+# print(num1 == num2)
+# print(num1 != num2)
+# print(num1 > num2)
+# print(num1 < num2)
+# print(num1 >= num2)
+# print(num1 <= num2)
+
+# # 10. Compare two cities.
+# city1 = input("First City:")
+# city2 = input("Second City:")
+
+# print(city1 > city2)
+# print(city1 < city2)
+# print(city1 == city2)
+# print(city1 != city2)
+# print(city1 > city2)
+# print(city1 < city2)
+
+# 11. Compare the length of two names using len()
+
+first = "Rudra"
+second = "Aanchal"
+print(len(first) == len(second))
+print(len(first) != len(second))
+print(len(first) > len(second))
+print(len(first) < len(second))
+print(len(first) <= len( second))
+print(len(first) >= len(second))
+
+# 12. Compare two mobile numbers (as strings)
+a = "4567890"
+b = "2346534"
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+print(a < b)
+print(a > b )
+
+
+
+

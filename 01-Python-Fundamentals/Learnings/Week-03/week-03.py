800401875ba64a567b18ab83dbc9c5608e84b4e5
Rudra Pratap Singh Chauhan <rudrap404@gmail.com>
2026-08-06 20:28:52 +0530

    Add Week 3 loop practise questions

---
+++ 01-Python-Fundamentals/Learnings/Week-03/week-03.py
@@ -0,0 +1,106 @@
+# For loop
+
+# 1.   6 at a time
+
+for i in range(7):
+    print(i)
+
+# 2.  range(start, stop)
+
+for i in range(2,7):
+    print(i)
+
+# 3.   range(start, stop, step)
+
+for i in range(2,11,2):
+    print(i)
+
+
+# 4. Reverse counting range(10, 0, -1)
+
+for i in range(10, 0 ,-1):
+    print(i)
+
+# 5. Print numbers from 1 to 10 
+
+for i in range(1,11,1):    
+    print(i)
+
+# 6. Print numbers from 10 to 1.
+
+for i in range(10,0,-1):
+    print(i)
+
+# 7. Print your name 10 times.
+
+for i in range(5):
+    print("Rudra")
+
+
+# 8. Print numbers from 1 to 50
+
+for i in range(1,50):
+    print(i)
+
+
+# 9. Print numbers from 50 to 1
+
+for i in range(50,0,-1):
+    print(i)
+
+# 10. Print even numbers from 1 to 20.
+
+for i in range(0,20,2):
+    print(i) 
+
+
+# 11. Print odd numbers from 1 to 20.
+
+for i in range(1,20,2):
+    print(i)
+
+
+# 12. Print multiples of 5 till 100.
+
+for i in range(5,105,5):
+    print(i)
+
+# 13. Print squares from 1 to 20.
+
+for i in range(1, 21):
+    print(i**2)
+
+
+# 14. Print cubes from 1 to 15.
+
+for i in range(1,16):
+    print(i**3)
+
+# 15. . Print multiplication table of 2
+
+for i in range(1,11):
+
+    print(i*2)
+
+# 16. Print first 25 natural numbers.
+
+for i in range(1,26):
+    print(i)
+
+# 17. Print numbers divisible by 3
+
+for i in range(3,100,3):
+    print(i)
+
+# 18. Print numbers divisible by both 2 and 5
+
+for i in range(1,101):
+    if i % 2 == 0 and i % 5 == 0:
+        print(i)
+
+# 19. Sum of numbers from 1 to 100.
+
+for i in range(1,101):
+    print(i+1)
+
+

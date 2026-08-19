1ef6861340ea8d6d19fb816281018afad5e69561
Rudra Pratap Singh Chauhan <rudrap404@gmail.com>
2026-08-19 22:04:50 +0530

    Adding practise question of Week 4

--- 01-Python-Fundamentals/Learnings/Week-04/week-04.py
+++ 01-Python-Fundamentals/Learnings/Week-04/week-04.py
@@ -123,6 +123,245 @@ def sum():
 
 sum()    
 
+# 15. Function to print squares from 1 to 20.
+
+def square():
+    for i in range(1,21):
+        print(f"The square of {i} is {i **2}")
+
+
+square()
+
+# 16. Function to print cubes from 1 to 10.
+
+def cube():
+    for i in range(1,11):
+        print(f"The cube of {i} is {i**3}")
+
+cube()        
+
+
+# 17. Function to print numbers in reverse.
+
+def reverse():
+    for i in range(100,0,-1):
+        print(f"reverse order  {i}")
+
+reverse()
+
+# 18. Function to print all numbers divisible by 3
+
+def divisible():
+    for i in range(1,100):
+        if i%3 == 0:
+            print(f"divisible by 3 is {i}")
+
+divisible()
+
+# Parameters & Arguments
+
+
+# 19. Function that takes a name and prints a greeting.
+
+def greeting(name):
+    print(f"Hello! {name}, Welcome back")
+
+greeting("RUDRA PRATAP SINGH CHAUHAN")    
+
+# 20. Function that takes age and prints age category.
+
+def ageCategory(age):
+    print(f"Hello! my age is {age}")
+
+ageCategory(21)    
+
+# 21. Function that takes two numbers and prints their sum.
+
+def Arithematic(a,b):
+    print(f"Sum of a and b is {a+b}")
+    print(f"Subtraction of a and b is {a - b}")
+    print(f"multiply of a and b is {a*b}")
+    
+ 
+
+Arithematic(a = 12,b=25)    
+
+# 22 Function that takes a number and prints its table.
+
+def table(a):
+    print(f)
+    for i in range(a,a*10+1,a):
+        print(f"{i}")
+
+table(2)
+
+# 23.Create a function that prints "Welcome to AI Engineering".
+
+def greet():
+    print("Welcome to AI Engineering")
+
+
+greet()
+
+
+# 24. Create a function that prints a formatted student profile. 
+
+def studentprofile():
+    nam = input("Student Name:")
+    course = input("Course:")
+    Year = int(input("Enter year"))
+    print()
+    print("="*40)
+    print(f"Student")
+    print("="*40)
+    print()
+    print(f"Student Name:{nam}")
+    print(f"Course Name :{course}")
+    print(f"Year        : {Year}")
+
+
+
+
+studentprofile()
+
+
+
+# 25. Function that takes a number and checks whether it is even or odd.
+
+def checker(a):
+    if a % 2 == 0:
+        print(f"It's a Even Number {a}")
+    else:
+        print(f"It's a Odd Number {a}")
+
+
+checker(5)
+
+
+# 26. Function that takes marks and prints grade
+
+def marks(math,science,english):
+    total = math + science+ english
+    print(f"Total Marks {total}")
+    if total >= 300:
+        print("A+")
+    elif total >= 250:
+        print("A")
+    elif total >= 200:
+        print("B+")
+    elif total >= 150:
+        print("B")
+    elif total >= 100:
+        print("C+")
+    else:
+        print("C, Means u are fail")
+
+
+marks(math = 10,science = 100,english = 60)
+
+
+# 27. Function that takes age and determines voting eligibility.
+
+def votingeligiblity(age):
+    print("Voting Eligiblity")
+    if age >= 18:
+        print("You are Eligible for Voting")
+    else:
+        print("You are Not Eligible for Voting")
+
+votingeligiblity(20)
+
+
+# 28. Function that takes percentage and returns grade category.
+
+def sum(a,b):
+    return a + b
+
+
+print(sum(30,90))
+
+# 29. Function returning square.
+
+def square(a):
+    return a**2
+
+print(square(7))
+
+# 30. Function returning even/odd status.
+
+def check(a):
+    if a % 2 == 0:
+        return "It's a Even Number"
+    else:
+        return "it's a Odd Number"
+
+print(check(9))    
+
+
+# 31. Function returning largest of two numbers.
+
+def secondlargest(a:float,b:float):
+    return a if a > b  else b
+
+
+print(secondlargest(255,500))
+
+
+# 32. Function returning student's grade.
+
+def studentgrade(name,college,course):
+    return name,college,course  
+
+print(studentgrade(name ="Rudra",college = "IIT Jodhpur",course ="BS"))
+
+
+# Functions + Conditions + Loops
+
+# 33. Function that prints even numbers up to n.
+
+def check(a):
+    for i in range(2,a+1,2):
+        print(i)
+
+
+check(30)
+
+# 34. Function that prints numbers in reverse.
+
+def reverse(a):
+    for i in range(a,0,-1):
+        print(i)
+
+reverse(100)
+
+# 35. Function that calculates sum up to n.
+
+def sum(a):
+    sum = 0
+    for i in range(0,a+1):
+         sum = sum + i 
+         print(sum)
+
+    
+
+sum(80)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
 
 
 

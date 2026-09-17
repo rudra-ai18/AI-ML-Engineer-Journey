1e8c03ba782f064c92cd9f05f195b9d5df3eabb2
Rudra Pratap Singh Chauhan <rudrap404@gmail.com>
2026-09-17 20:29:14 +0530

    Initial commit

---
+++ Python/01-Python-Fundamentals/Learnings/Week-06/week-06.py
@@ -0,0 +1,76 @@
+name = "Rudra Pratap Singh"
+print(name)
+
+
+
+# 2. Print the length of your name.
+
+# name = input("ENTER YOUR NAME:")
+# print(len(name))
+
+# 3. Print each character of your name using its index. 
+
+# name = "Rudra"
+# print(name[0:])
+
+# 4.  Print characters from index 0 to 4.
+
+# name = "Rudra Pratap Singh"
+# print(name[0:5])
+
+# 5. Take a name and print it in reverse.
+
+# name ="Rudra"
+# print(name[::-1])
+
+# 6.  Take a sentence and print only characters at even indexes.
+
+# senten = "I'm goood boy"
+# print(senten[0::2])
+
+
+# 7. Check whether a string is a palindrome using slicing.
+
+# my_string = input("Enter")
+# if my_string == my_string[::-1]:
+#     print(f"This is a {my_string} Palindrome")
+# else:
+#     print(f"This is not {my_string} Palindrome")
+
+# 8. Convert a name to uppercase.m
+
+# name  = "mudit badnaam"
+# print(name.upper())
+
+# 9.Convert a name to lowercase.
+
+# name = "RUDRA"
+# print(name.lower())
+
+# 10. Convert a sentence to title case.
+
+# title = "college student"
+# print(title.title())
+
+# 11. 4. Capitalize a sentence.
+
+# title = "python is the best language"
+# print(title.upper())
+
+# 12. Remove extra spaces from a string.
+
+# name = "  Python"
+# print(name.strip())
+
+
+
+# 14.  Replace a particular word with another word.
+
+# fruits = ("apple is best fruit")
+# print(fruits.replace("apple","orange"))
+
+
+# 15. Convert "hello python" into "Hello Python".
+
+somet_ = "hello python"
+print(somet_.title())
